1e953b981fa21bcb8bcfc25d7c55e545bc92e57b
shigehito7110 <shige.7110.330@gmail.com>
2021-08-20 05:02:47 +0900

    [Create] validator作成

--- src/enigoma.go
+++ src/enigoma.go
@@ -1,9 +1,40 @@
-package main
+package enigoma
 
-import (
+import(
 	"fmt"
+	vl "github.com/shigehito7110/enigoma/validator"
 )
 
-func main () {
-	fmt.Println("hello enigoma!!")
+type Enigoma struct {
+	Password string
+	Validator vl.Validator
+}
+
+type validator func() vl.Validator
+
+func NewEnigoma(p string, vs ...validator) (*Enigoma, error){
+	e := &Enigoma{
+		Password: p,
+	}
+
+	switch(len(vs)) {
+		case 0:
+			e.Validator = vl.NewDefaultPasswordValidator()
+		case 1:
+			for _, v := range vs {
+				e.Validator = v()
+			}
+		default:
+			return nil, fmt.Errorf("Too many arguments,given %d expected 1", len(vs))
+	}
+
+	return e, nil
+}
+
+func (e *Enigoma) Validate() error {
+	if result := e.Validator.Validate(e.Password); result {
+		return nil
+	}
+
+	return fmt.Errorf("%s is invalid values for password", e.Password)
 }
